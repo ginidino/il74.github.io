a3e10336df3d6baf609f43be743c7f044b17ce0d
Injae Lee <94044443+ginidino@users.noreply.github.com>
2024-05-11 17:25:57 +0100

    Update story.js

--- js/story.js
+++ js/story.js
@@ -59,7 +59,13 @@ function displayStories() {
                 "<p>" + story.content + "</p>" +
                 "<p><strong>Category:</strong> " + story.category + "</p>" +
                 "<button class='edit-btn' onclick='editStory(" + index + ")'>Edit</button>" +
-                "<button class='delete-btn' onclick='deleteStory(" + index + ")'>Delete</button>";
+                "<button class='delete-btn' onclick='deleteStory(" + index + ")'>Delete</button>" +
+                "<button class='comment-btn' onclick='toggleCommentForm(" + index + ")'>Comment</button>" +
+                "<div id='commentForm-" + index + "' class='comment-form' style='display: none;'>" +
+                    "<label for='commentContent-" + index + "'>Comment:</label><br>" +
+                    "<textarea id='commentContent-" + index + "' rows='4' cols='50'></textarea><br>" +
+                    "<button onclick='submitComment(" + index + ")'>Submit Comment</button>" +
+                "</div>";
 
             if (story.image) {
                 storiesHTML += "<p>Image Selected</p>";
@@ -75,6 +81,43 @@ function displayStories() {
 }
 
 displayStories();
+
+function toggleCommentForm(index) {
+    var commentForm = document.getElementById("commentForm-" + index);
+    if (commentForm.style.display === "none") {
+        commentForm.style.display = "block";
+    } else {
+        commentForm.style.display = "none";
+    }
+}
+
+function submitComment(index) {
+    var commentContent = document.getElementById("commentContent-" + index).value;
+    if (commentContent.trim() !== "") {
+        var storiesList = JSON.parse(localStorage.getItem("stories")) || [];
+        var story = storiesList[index];
+        if (!story.comments) {
+            story.comments = [];
+        }
+        story.comments.push(commentContent);
+        localStorage.setItem("stories", JSON.stringify(storiesList));
+        displayComments(index);
+    }
+}
+
+function displayComments(index) {
+    var storiesList = JSON.parse(localStorage.getItem("stories")) || [];
+    var story = storiesList[index];
+    var commentsHTML = "";
+    if (story.comments && story.comments.length > 0) {
+        commentsHTML += "<div class='comments'><h3>Comments</h3><ul>";
+        story.comments.forEach(function(comment) {
+            commentsHTML += "<li>" + comment + "</li>";
+        });
+        commentsHTML += "</ul></div>";
+    }
+    document.getElementById("story-" + index).innerHTML += commentsHTML;
+}
 
 document.getElementById("categoryList").addEventListener("change", function() {
     var selectedCategory = this.value;
